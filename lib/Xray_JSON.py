92a1c919b2422150e16872658d7dfe82115826b5
AngularLock <resnick.xing@aol.com>
2019-04-18 00:11:28 +0800

    add json exampler

---
+++ lib/Xray_JSON.py
@@ -0,0 +1,14 @@
+import json
+
+class JSON:
+    def __init__(self):
+        pass
+
+    def JSONRead(self,path):
+        with open(path, 'r') as json_f:
+            data = json.load(json_f)
+        return data
+
+    def JSONWrite(self,path,dict_info):
+        with open(path,'w') as json_f:
+            json.dump(dict_info, json_f)
